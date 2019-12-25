481a59d28ae8cf0fffd2073c06e7b7a16f15305f
codingimages <rnavedojr@outlook.com>
2019-12-24 19:22:16 -0500

    Updated service workers for PWA functionality

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -34,18 +34,30 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys()
+      .then(keyList => Promise.all(keyList.map((key) => {
+        if (key != cacheName) {
+          console.log("Service Worker Removing Old Cache", key)
+          return caches.delete(key)
+        }
+      })))
+  )
+})
+
 /* Serve cached content when offline */
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
